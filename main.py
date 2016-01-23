f82f7cc63c3dd76eabd34d869ba5264323e874ac
Adam Long <macadam.long@gmail.com>
2016-01-23 00:33:05 +0000

    faster, threads

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from telegram import Updater
 import telegram
 import logging
 import os
+import threading
 
 logging.basicConfig(
 	format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 	level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+thread_id = 0
 
 
 def start(bot,update):
@@ -16,39 +19,46 @@ def start(bot,update):
 def error(bot,update,error):
 	logger.warn('Update "%s" caused error "%s"' % (update, error))
 
-def returnMusic(bot, update):
+def startReturnThread(bot, update):
+	global thread_id
+	t=threading.Thread(target=returnMusic,args=(bot,update,thread_id))
+	t.daemon=True
+	t.start()
+	thread_id+=1
+
+def returnMusic(bot, update, thread_idd):
 	u=update
 	id = (u.message.chat_id)
 	update_id = (u.update_id)
 	last_id=update_id
 	try:
-		cleanVideo()
-		title=os.popen("./youtube-dl -e "+u.message.text).read()
+		title=os.popen("./youtube-dl -e "+u.message.text).read()+str(thread_idd)
+		cleanVideo(title)
 		open("current_title",'w').write(str(title))
 		title=os.popen("cat current_title | tr -dc '[:alnum:]'").read()
 		bot.sendMessage(id,"Downloading video: "+title)
-		downloadVideo(u.message.text)
+		downloadVideo(u.message.text,title)
 		bot.sendMessage(id,"Converting video: "+title)
-		convertVideo()
+		convertVideo(title)
 		renameVideo(title)
 		bot.sendChatAction(chat_id=id, action=telegram.ChatAction.UPLOAD_AUDIO)
 		bot.sendAudio(id,open(title+".mp3","r"))
-		cleanVideo()
+		cleanVideo(title)
 	except Exception, e:
 		bot.sendMessage(id,"there was a problem")
 		bot.sendMessage(id,str(e))
-		cleanVideo()
+		cleanVideo(title)
 		print("There was a problem")
 		print("ID:"+str(last_id))
 		open("update_id",'w').write(str(last_id))
 
-def downloadVideo(url):
-	command="./youtube-dl -o tmpa "+url
+def downloadVideo(url, title):
+	command="./youtube-dl -o "+title+" "+url
 	print(command)
 	os.system(command)
 
-def convertVideo():
-	command="ffmpeg -i tmpa.* a.mp3"
+def convertVideo(title):
+	command="ffmpeg -i "+title+".* a.mp3"
 	print(command)
 	os.system(command)
 
@@ -57,8 +67,8 @@ def renameVideo(title):
 	print(command)
 	os.system(command)
 
-def cleanVideo():
-	command="rm *.mp3 tmpa.* tmp.*"
+def cleanVideo(title):
+	command="rm "+title+".*"
 	os.system(command)
 
 def main():
@@ -66,7 +76,7 @@ def main():
 	dp=updater.dispatcher
 	dp.addTelegramCommandHandler("start", start)
 
-	dp.addTelegramMessageHandler(returnMusic)
+	dp.addTelegramMessageHandler(startReturnThread)
 
 	dp.addErrorHandler(error)
 
